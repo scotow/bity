4221292f2ed0f4ee4d05a78e65d6bd6f53792346
scotow <contact@scotow.com>
2024-06-03 00:47:46 +0200

    Add format function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 pub fn parse(input: &str) -> Result<u64, ()> {
     if !input.is_ascii() {
         return Err(());
@@ -53,6 +55,33 @@ pub fn parse(input: &str) -> Result<u64, ()> {
         + apply_unit(fraction_str, unit, 10u64.pow(fraction_str.len() as u32))?)
 }
 
+pub fn format(input: u64) -> String {
+    if input == 0 {
+        return "0b".to_owned();
+    }
+
+    let exponent = input.ilog10() / 3;
+    let unit = match exponent {
+        0 => "",
+        1 => "k",
+        2 => "M",
+        3 => "G",
+        4 => "T",
+        5 => "P",
+        6 | _ => "E"
+    };
+
+    let mut output = String::with_capacity(8);
+    let exponent_base = 10u64.pow(exponent * 3);
+    let integer = input / exponent_base;
+    write!(output, "{integer}").expect("write error");
+    if input % exponent_base != 0 {
+        write!(output, ".{0:.2}", (input % exponent_base).to_string().trim_end_matches('0')).expect("write error");
+    }
+    write!(output, "{unit}b").expect("write error");
+    output
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -94,5 +123,28 @@ mod tests {
         assert_eq!(super::parse(" 12kb").unwrap(), 12_000);
         assert_eq!(super::parse("12kb ").unwrap(), 12_000);
         assert_eq!(super::parse("12 kb").unwrap(), 12_000);
+    }
+
+    #[test]
+    fn format() {
+        assert_eq!(super::format(0), "0b");
+        assert_eq!(super::format(1), "1b");
+        assert_eq!(super::format(12), "12b");
+        assert_eq!(super::format(123), "123b");
+        assert_eq!(super::format(1_234), "1.23kb");
+        assert_eq!(super::format(12_000), "12kb");
+        assert_eq!(super::format(12_345), "12.34kb");
+        assert_eq!(super::format(123_456), "123.45kb");
+        assert_eq!(super::format(1_234_567), "1.23Mb");
+        assert_eq!(super::format(12_345_678), "12.34Mb");
+        assert_eq!(super::format(123_456_789), "123.45Mb");
+        assert_eq!(super::format(1_234_567_891), "1.23Gb");
+        assert_eq!(super::format(12_345_678_912), "12.34Gb");
+        assert_eq!(super::format(12_300_000_000_000), "12.3Tb");
+        assert_eq!(super::format(12_300_000_000_000_000), "12.3Pb");
+        assert_eq!(super::format(12_300_000_000_000_000_000), "12.3Eb");
+
+        // Extra.
+        assert_eq!(super::format(1_200), "1.2kb");
     }
 }
