d476da1aa7de71f334b445549d4a5039983f6d60
scotow <contact@scotow.com>
2025-05-24 22:05:36 +0200

    Move lints to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -102,43 +102,8 @@
 //! - No customizable formating
 //! - `u64` limited (doesn't go above *exa*, aka. `10^18`)
 
-#![warn(
-    clippy::all,
-    clippy::todo,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::mem_forget,
-    clippy::unused_self,
-    clippy::filter_map_next,
-    clippy::needless_continue,
-    clippy::needless_borrow,
-    clippy::match_wildcard_for_single_variants,
-    clippy::if_let_mutex,
-    clippy::await_holding_lock,
-    clippy::match_on_vec_items,
-    clippy::imprecise_flops,
-    clippy::suboptimal_flops,
-    clippy::lossy_float_literal,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::fn_params_excessive_bools,
-    clippy::exit,
-    clippy::inefficient_to_string,
-    clippy::linkedlist,
-    clippy::macro_use_imports,
-    clippy::option_option,
-    clippy::verbose_file_reads,
-    clippy::unnested_or_patterns,
-    clippy::str_to_string,
-    future_incompatible,
-    missing_debug_implementations,
-    missing_docs,
-    nonstandard_style,
-    rust_2018_idioms,
-    unexpected_cfgs
-)]
-#![deny(unreachable_pub)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![cfg_attr(not(test), warn(clippy::print_stdout, clippy::dbg_macro))]
+#![cfg_attr(not(test), warn(clippy::dbg_macro, clippy::print_stdout))]
 
 pub mod bit;
 pub mod bps;
